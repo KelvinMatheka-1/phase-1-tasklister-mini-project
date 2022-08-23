c8fa01875e03ed5b5f10483cd24a9fe68c583b28
Kelvin M Matheka <beingmatheka@gmail.com>
2022-08-23 19:21:34 +0300

    Done with assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.getElementById("create-task-form");
+  form.addEventListener("submit", addTask);
 });
+
+function addTask(e){
+  e.preventDefault();
+  const task = e.target.querySelector('#new-task-description').value;
+  let listTask = document.createElement("li");
+  listTask.innerText = task;                     
+  document.getElementById('tasks').appendChild(listTask);
+  e.target.querySelector('#new-task-description').value = '';
+}
